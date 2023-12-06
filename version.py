d8d4d2e6aaf6d55fdb8c65481bf9844ec9631992
Your Name <chandanhans2003@gmail.com>
2023-12-06 21:29:11 +0530

    final

--- version.py
+++ version.py
@@ -59,7 +59,7 @@ def check_for_updates():
     time_difference = remote_version_date - local_version_date
 
     # Check if the difference is greater than 5 minutes
-    if time_difference > datetime.timedelta(minutes=5):
+    if time_difference > datetime.timedelta(minutes=2):
         print(f"Update available. Initiating update...")
         new_exe_path = download_new_version()
         initiate_update(new_exe_path)
