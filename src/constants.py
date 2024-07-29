761ee745b8741336d57981fd4b7864d4fe234c3e
ChandanHans <chandanhans2003@gmail.com>
2024-07-29 17:46:57 +0530

    removed unused files

--- src/constants.py
+++ src/constants.py
@@ -2,10 +2,7 @@ from src.utils import resource_path
 
 
 SIGNATURE_TEMPLATE_PATH = resource_path("templates/signature.html")
-CLIENT_EMAIL_TEMPLATE_PATH = resource_path("templates/client_email.html")
 NOTARY_EMAIL_TEMPLATE_PATH = resource_path("templates/notary_email.html")
-FACTURE_EMAIL_TEMPLATE_PATH = resource_path("templates/facture_email.html")
-FACTURE_TEMPLATE_PATH = resource_path("templates/template.docx")
 
 INTERN_SHEET_KEY = "1cveFT3BvSJ9d-PvBdyGvZ7Fd_0XYDlNafOpTXUiz_eI"
 NOTARY_SHEET_KEY = "1C-5OCv2Nvkr8ZSrfpnO1D5K8-kzybsu5bUa6eQL6Bj0"
